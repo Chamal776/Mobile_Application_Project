674af57999c028b147970cfc5d0e34145c45f881
Chamal776 <chamalharshana2020@gmail.com>
2026-01-28 15:26:18 +0530

    Created Main Test Page

--- projectapp/lib/main.dart
+++ projectapp/lib/main.dart
@@ -2,7 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+}import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: mainHome()));
 }
+
+class mainHome extends StatelessWidget {
+  const mainHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Center(
+          child: Text(
+            "project 1",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w700,
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+
+      body: mainBody(),
+    );
+  }
+}
+
+class mainBody extends StatefulWidget {
+  const mainBody({super.key});
+
+  @override
+  State<mainBody> createState() => _mainBodyState();
+}
+
+class _mainBodyState extends State<mainBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+        
+      ],
+    );
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
